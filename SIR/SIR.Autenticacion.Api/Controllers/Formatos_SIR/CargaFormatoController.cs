59c5f5befb839124636ce0d16ed3dd845c42a009
sergionet17 <sergionet17@gmail.com>
2021-05-19 21:06:26 -0500

    Se agrega logicca de cargfa de formatos
    carga de formatos

---
+++ SIR/SIR.Autenticacion.Api/Controllers/Formatos_SIR/CargaFormatoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using SIR.Comun.Entidades.Formatos_SIR;
+
+namespace SIR.Autenticacion.Api.Controllers.Formatos_SIR
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CargaFormatoController : ControllerBase
+    {
+        [HttpPost]
+        public MOD_Carga_Formato CargarFormatos(string filtro)
+        {
+            MOD_Carga_Formato mOD_Carga = new MOD_Carga_Formato();
+            mOD_Carga.Id = "4";
+            mOD_Carga.detalle = "este es el valor";
+            //var context = FabricaNegocio.CrearClienteExcluido;
+            return mOD_Carga;
+        }
+
+    }
+}
---
+++ SIR/SIR.Comun/Entidades/Formatos_SIR/MOD_Carga_Formato.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SIR.Comun.Entidades.Formatos_SIR
+{
+    [DataContract]
+    [Serializable]
+    public class MOD_Carga_Formato
+    {
+        [DataMember]
+        public string Id { get; set; }
+        [DataMember]
+        public string detalle { get; set; }
+    }
+}
